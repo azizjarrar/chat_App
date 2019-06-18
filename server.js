5faedbd0563d2b6cb080ab265ffc536b3ad15f53
azizjarrar <aziz_jarrar@hotmail.com>
2019-06-18 15:19:36 +0200

    chat app  work local

--- server.js
+++ server.js
@@ -1,49 +1,63 @@
-/*
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://tripelbil:<password>@cluster0-mfk2y.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-*/
-
-/*
-const MongoClient = require('mongodb').MongoClient;
-
-// replace the uri string with your connection string.
-const uri = "mongodb+srv://azizjarrar:<a1z2e3a4>@cluster0-mfk2y.mongodb.net/test?retryWrites=true&w=majority"
-MongoClient.connect(uri,{ useNewUrlParser: true } ,function(err, client) {
-    console.log(client)
-    if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-
-});
-*/
 var MongoClient = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+var http = require('http');
+//connect to mongo
 var url = "mongodb+srv://unha:unha280198@cluster0-mfk2y.mongodb.net/test?retryWrites=true&w=majority";
-MongoClient.connect(url, { useNewUrlParser: true },function(err, client) {
+MongoClient.connect(url, { useNewUrlParser: true },function(err, cl) {
 if(err){
     throw err;
 }else{
     console.log("connextion secc");
+    // connect to socket.io
+    client.on('connection',(socket)=>{
+       let db = cl.db('chat_app');
+       let chat = db.collection('chats');
+    // Create function to send status
+    sendStatus = (s)=>{
+        socket.emit('status',s)
+    }  
+    //Get chats from mongo collection 
+    chat.find().limit(100).sort({_id:1}).toArray((err,res)=>{
+        if(err){
+            throw err
+        }else{
+            //emit the messages
+            socket.emit('output',res)
+        }
+    });
+    //Handle input events
+    socket.on('input',(data)=>{
+        let name=data.name;
+        let message = data.message
+        //check for name and message
+        if(name == '' || message == ''){
+            // send error status
+            sendStatus('please enter a name and message');
+        }else{
+            // insert message
+            chat.insert({name:name,message:message},()=>{
+                client.emit('output',[data]);
+            // send status object
+            sendStatus({message:"Message sent",clear:true})
+            })  
+        }
+    });
+    // handle clear
+    socket.on('clear',()=>{
+        //remove all chats from collection
+        chat.remove({},()=>{
+            // emit clear
+            socket.emit('cleard')
+        });
+     });
+    })
 }
 });
-
-
-
-
-
-    /*
-const password ="a1z2e3a4"
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://tripelbil:"+password+"@cluster0-mfk2y.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-if(err){
-    throw err;
-}
-});
+/*
+http.createServer((req, res)=>{
+    res.write('Hello World!');
+}).listen(3000)
 */
+
+
+
